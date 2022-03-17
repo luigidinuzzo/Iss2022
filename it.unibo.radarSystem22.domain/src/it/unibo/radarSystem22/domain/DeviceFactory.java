6ddfa043d1b894fa99ed84375addb0a3281e18c7
Luigi di Nuzzo <luigidinuzzo@MacBook-Pro-di-Luigi.local>
2022-03-17 13:55:31 +0100

    aggiornamento

--- it.unibo.radarSystem22.domain/src/it/unibo/radarSystem22/domain/DeviceFactory.java
+++ it.unibo.radarSystem22.domain/src/it/unibo/radarSystem22/domain/DeviceFactory.java
@@ -4,10 +4,12 @@ import it.unibo.radarSystem22.domain.interfaces.ILed;
 import it.unibo.radarSystem22.domain.interfaces.ISonar;
 import it.unibo.radarSystem22.domain.models.LedModel;
 import it.unibo.radarSystem22.domain.models.SonarModel;
+import it.unibo.radarSystem22.domain.utils.ColorsOut;
 
 public class DeviceFactory {
 
 	public static ILed createLed() {
+		ColorsOut.out("MIAO", ColorsOut.BgGreen);
 		return LedModel.create();
 	}
 
--- it.unibo.radarSystem22.domain/test/it/unibo/radarSystem22/domain/TestLed.java
+++ it.unibo.radarSystem22.domain/test/it/unibo/radarSystem22/domain/TestLed.java
@@ -1,35 +1,61 @@
 package it.unibo.radarSystem22.domain;
 
 import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.junit.*;
 
 import it.unibo.radarSystem22.domain.interfaces.ILed;
-import it.unibo.radarSystem22.domain.mock.LedMock;
+import it.unibo.radarSystem22.domain.utils.BasicUtils;
+import it.unibo.radarSystem22.domain.utils.DomainSystemConfig;
  
 
 
 public class TestLed {
+ 
+	@Before
+	public void up() {
+		System.out.println("up");
+	}
 	
-	@Test 
-	public void testLedMockOn() {
-		System.out.println("testLedMockOn");
-		
-		ILed led =  new LedMock();
-		led.turnOn();
-		
-		assertTrue(led.getState());	
-		
+	@After
+	public void down() {
+		System.out.println("down");		
 	}	
 	
-	@Test
-	public void testLedMockOff() {
-		System.out.println("testLedMockOff");
+	@Test 
+	public void testLedMock() {
 		
-		ILed led =  new LedMock();
-		led.turnOff();
+		System.out.println("testLedMock");
+		DomainSystemConfig.simulation = true; 
 		
-		assertTrue(!led.getState());	
-	}
+		ILed led = DeviceFactory.createLed();
+		assertTrue( ! led.getState() );
+		
+ 		led.turnOn();
+		assertTrue(  led.getState() );
+		
+		BasicUtils.delay(1000);		//to see the ledgui
+		
+ 		led.turnOff();
+		assertTrue(  ! led.getState() );	
+		
+		BasicUtils.delay(1000);		//to see the ledgui
+	}	
+	
+	//@Test 
+	public void testLedConcrete() {
+		
+		System.out.println("testLedConcrete");
+		DomainSystemConfig.simulation = false; 
+		
+		ILed led = DeviceFactory.createLed();
+		assertTrue( ! led.getState() );
+		
+ 		led.turnOn();
+		assertTrue(  led.getState() );
+		
+ 		led.turnOff();
+		assertTrue(  ! led.getState() );		
+	}		
+	
  
 }
